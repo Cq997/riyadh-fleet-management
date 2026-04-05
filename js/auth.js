3b52b199ab2ed1c2779fcebd457423927fcca198
Cq997 <C.cq997@gmail.com>
2026-04-05 22:53:32 +0300

    Update login function to use new input IDs

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 function login() {
-  const user = document.getElementById("srca").value;
-  const pass = document.getElementById("997").value;
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
 
   if (user === "admin" && pass === "1234") {
     localStorage.setItem("auth", true);
