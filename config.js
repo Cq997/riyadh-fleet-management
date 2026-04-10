fbb6bdc488c8fcac55fdc74ed76c62f48878066a
Cq997 <C.cq997@gmail.com>
2026-04-10 15:35:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbykN_PVvM0niqmp-2se9F5bcjw7Z2-o4-tiFOQDSHVEYIKnGWdG5rUBrjVfR55YAA3_ng/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzivVOrGZSCoOW7MlLOywUMVFjOdkMgOFl54QhpGZIkDdc-FFAq21zuchpMXQ_XLDoMDQ/exec";
